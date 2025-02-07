00ad76c0ef3a6a46b102a29c770d2d568f74e2ca
Benício Januário <pbenicio.januario@gmail.com>
2025-02-07 14:33:41 -0300

    test: creation, deletion and filter fetch

--- internal/server/routes_test.go
+++ internal/server/routes_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"device-api/internal/model"
 	"encoding/json"
 	"net/http"
@@ -75,6 +76,125 @@ func TestFetchDeviceHandler(t *testing.T) {
 				assert.NoError(t, err)
 				assert.Equal(t, existingDevice.ID, fetchedDevice.ID)
 				assert.Equal(t, existingDevice.Name, fetchedDevice.Name)
+			}
+		})
+	}
+}
+
+func TestCreateDeviceHandler(t *testing.T) {
+	handler := NewRequestHandler()
+
+	tests := []struct {
+		name           string
+		payload        string
+		expectedStatus int
+	}{
+		{"Valid device creation", `{"name": "Some Device", "brand": "Some Brand"}`, http.StatusCreated},
+		{"Missing name", `{"brand": "Some Brand"}`, http.StatusBadRequest},
+		{"Missing brand", `{"name": "Some Device"}`, http.StatusBadRequest},
+		{"Missing both name and brand", `{}`, http.StatusBadRequest},
+		{"Invalid JSON", `{"name": "Some Device", "brand": "Some Brand"`, http.StatusBadRequest},
+		{"Invalid fields", `{"nmae": "Some Device", "brandy": "Some Brand"`, http.StatusBadRequest},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			setupTest(t)
+
+			req := httptest.NewRequest("POST", "/devices", bytes.NewBufferString(tc.payload))
+			req.Header.Set("Content-Type", "application/json")
+			w := httptest.NewRecorder()
+
+			handler.ServeHTTP(w, req)
+
+			assert.Equal(t, tc.expectedStatus, w.Code)
+		})
+	}
+}
+
+func TestDeleteDeviceHandler(t *testing.T) {
+	setupTest(t)
+
+	handler := NewRequestHandler()
+
+	existingDevice := model.Device{ID: uuid.New(), Name: "Device", Brand: "Brand", State: string(model.AVAILABLE)}
+	inUseDevice := model.Device{ID: uuid.New(), Name: "Someone's device", Brand: "Brand", State: string(model.IN_USE)}
+
+	devices[existingDevice.ID] = existingDevice
+	devices[inUseDevice.ID] = inUseDevice
+
+	tests := []struct {
+		name           string
+		deviceID       string
+		expectedStatus int
+	}{
+		{"Device exists", existingDevice.ID.String(), http.StatusNoContent},
+		{"Cannot delete in-use devices", inUseDevice.ID.String(), http.StatusBadRequest},
+		{"Device not found", uuid.New().String(), http.StatusNotFound},
+		{"Invalid UUID", "invalid-uuid", http.StatusBadRequest},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			req := httptest.NewRequest("DELETE", "/devices/"+tc.deviceID, nil)
+			w := httptest.NewRecorder()
+
+			handler.ServeHTTP(w, req)
+
+			assert.Equal(t, tc.expectedStatus, w.Code)
+
+			if tc.expectedStatus == http.StatusNoContent {
+				_, exists := devices[existingDevice.ID]
+				assert.False(t, exists)
+			}
+		})
+	}
+}
+
+func TestFetchDevicesRoute(t *testing.T) {
+
+	handler := NewRequestHandler()
+
+	device1 := model.Device{ID: uuid.New(), Name: "Device1", Brand: "BrandA", State: string(model.AVAILABLE)}
+	device2 := model.Device{ID: uuid.New(), Name: "Device2", Brand: "BrandB", State: string(model.IN_USE)}
+	device3 := model.Device{ID: uuid.New(), Name: "Device3", Brand: "BrandA", State: string(model.IN_USE)}
+
+	tests := []struct {
+		name           string
+		queryParams    string
+		expectedStatus int
+		expectedCount  int
+	}{
+		{"Get all devices", "", http.StatusOK, 3},
+		{"Filter by brand", "?brand=BrandA", http.StatusOK, 2},
+		{"Filter by state", "?state=in-use", http.StatusOK, 2},
+		{"Filter by both brand & state", "?brand=BrandA&state=in-use", http.StatusOK, 1},
+		{"Invalid state filter", "?state=INVALID_STATE", http.StatusBadRequest, 0},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			setupTest(t)
+
+			devices[device1.ID] = device1
+			devices[device2.ID] = device2
+			devices[device3.ID] = device3
+
+			req := httptest.NewRequest("GET", "/devices"+tc.queryParams, nil)
+			w := httptest.NewRecorder()
+
+			handler.ServeHTTP(w, req)
+
+			assert.Equal(t, tc.expectedStatus, w.Code)
+
+			if tc.expectedStatus == http.StatusOK {
+				var fetchedDevices []model.Device
+				err := json.Unmarshal(w.Body.Bytes(), &fetchedDevices)
+				assert.NoError(t, err, "Error parsing response JSON")
+				assert.Equal(t, tc.expectedCount, len(fetchedDevices))
+			} else {
+				responseBody := strings.TrimSpace(w.Body.String())
+				assert.NotEmpty(t, responseBody, "Expected error message")
 			}
 		})
 	}
