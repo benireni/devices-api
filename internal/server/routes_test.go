5234632c576ef400c0881579aee888ee642fc94e
Benício Januário <pbenicio.januario@gmail.com>
2025-02-07 14:32:48 -0300

    test: fetch single device handler

--- internal/server/routes_test.go
+++ internal/server/routes_test.go
@@ -2,10 +2,12 @@ package server
 
 import (
 	"device-api/internal/model"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
@@ -32,4 +34,48 @@ func TestPingHandler(t *testing.T) {
 	responseBody := strings.TrimSpace(w.Body.String())
 
 	assert.JSONEq(t, expected, responseBody)
+}
+
+func TestFetchDeviceHandler(t *testing.T) {
+	setupTest(t)
+
+	handler := NewRequestHandler()
+
+	existingDevice := model.Device{
+		ID:           uuid.New(),
+		Name:         "Some Device",
+		Brand:        "Some Brand",
+		State:        string(model.AVAILABLE),
+		CreationTime: time.Now(),
+	}
+	devices[existingDevice.ID] = existingDevice
+
+	tests := []struct {
+		name           string
+		deviceID       string
+		expectedStatus int
+	}{
+		{"User exists", existingDevice.ID.String(), http.StatusOK},
+		{"User not found", uuid.New().String(), http.StatusNotFound},
+		{"Invalid UUID", "invalid-uuid", http.StatusBadRequest},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			req := httptest.NewRequest("GET", "/devices/"+tc.deviceID, nil)
+			w := httptest.NewRecorder()
+
+			handler.ServeHTTP(w, req)
+
+			assert.Equal(t, tc.expectedStatus, w.Code)
+
+			if tc.expectedStatus == http.StatusOK {
+				var fetchedDevice model.Device
+				err := json.Unmarshal(w.Body.Bytes(), &fetchedDevice)
+				assert.NoError(t, err)
+				assert.Equal(t, existingDevice.ID, fetchedDevice.ID)
+				assert.Equal(t, existingDevice.Name, fetchedDevice.Name)
+			}
+		})
+	}
 }
